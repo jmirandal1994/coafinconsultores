c2a3991071693b235caaadf219636d3651210a06
jmirandal1994 <jmiraandal@gmail.com>
2025-06-06 20:59:50 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '¡Hola desde Flask en Vercel!'
-
-# Este handler lo requiere Vercel
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
