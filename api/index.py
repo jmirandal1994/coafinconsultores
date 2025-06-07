0f5543e4b25dba873b91d3f74efcc8caa17050a8
jmirandal1994 <jmiraandal@gmail.com>
2025-06-06 21:09:57 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    return '¡Hola desde Flask en Vercel!'
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard/cliente.html')
